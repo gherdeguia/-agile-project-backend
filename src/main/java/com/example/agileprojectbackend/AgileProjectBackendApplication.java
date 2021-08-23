7a1a74a84584bdbde56d9ae0e046a220f67b1eb5
CANUTLO <lorenz.alvin.canuto@oocl.com>
2021-08-23 13:54:56 +0800

    Initialize Spring Boot Project

---
+++ src/main/java/com/example/agileprojectbackend/AgileProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.agileprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/agileprojectbackend/AgileProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agileprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
