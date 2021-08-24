d56082675bba741bda5eb75014783061355eca57
CANUTLO <lorenz.alvin.canuto@oocl.com>
2021-08-24 13:31:25 +0800

    [backend][Lorenz] feat: implemented CorsGlobalConfiguration

---
+++ src/main/java/com/example/agileprojectbackend/config/CorsGlobalConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.agileprojectbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class CorsGlobalConfiguration {
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+
+            }
+        };
+    }
+}
