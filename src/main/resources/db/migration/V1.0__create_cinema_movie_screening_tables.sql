7bf7492e9778b24675a61c50188584f27186067d
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-24 11:46:52 +0800

    [Backend][GG] refactor : fix flyway version conflicts

--- src/main/resources/db/migration/V1.0__create_cinema_movie_screening_tables.sql
+++ src/main/resources/db/migration/V1.0__create_cinema_movie_screening_tables.sql
@@ -1,20 +1,18 @@
 CREATE TABLE if NOT EXISTS Movie(
-  id               int not null  auto_increment primary key,
-  name             varchar(255) not null,
-  synopsis         varchar(255) not null,
+  id              int not null  auto_increment primary key,
+  name            varchar(255) not null,
+  synopsis        varchar(255) not null,
   directed_by      varchar(255) not null,
-  rated            varchar(255) not null,
+  rated           varchar(255) not null,
   running_time     datetime,
-  genre            varchar(255) not null,
+  genre           varchar(255) not null,
   release_date     date,
-  cast_and_crews   varchar(255) not null,
-  poster           varchar(255) not null,
-  status           varchar(255),
-  critics_rating   decimal(3,1),
-  audience_rating  decimal(3,1)
+  cast_and_crews  varchar(255) not null,
+  poster          varchar(255) not null
 );
+
 CREATE TABLE if NOT EXISTS Screening(
-  id               int not null  auto_increment primary key,
+  id              int not null  auto_increment primary key,
   available_seats  varchar(255) not null,
   start_time       datetime,
   end_time         datetime,
@@ -22,9 +20,10 @@ CREATE TABLE if NOT EXISTS Screening(
   movie_id         int,
   FOREIGN KEY (movie_id) REFERENCES Movie(id)
 );
+
 CREATE TABLE if NOT EXISTS Cinema(
-  id               int not null  auto_increment primary key,
-  name             varchar(255) not null,
+  id              int not null  auto_increment primary key,
+  name            varchar(255) not null,
   screening_id     int,
   movie_id         int,
   FOREIGN KEY (movie_id) REFERENCES Movie(id),
