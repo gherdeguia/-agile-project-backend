f9cab989b2aa1c66a27230ba00d58afdebf9bf36
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-24 23:18:40 +0800

    [Backend][GG] feat : created user_profile_table

--- src/main/resources/db/migration/V1.6__create_user_profiles_table.sql
+++ src/main/resources/db/migration/V1.6__create_user_profiles_table.sql
@@ -1,5 +1,6 @@
-CREATE TABLE if NOT EXISTS UserProfile(
+CREATE TABLE if NOT EXISTS Users(
   id                        int not null  auto_increment primary key,
+  user_name                 varchar(255) not null,
   first_name                varchar(255) not null,
   last_name                 varchar(255) not null,
   phone_number              varchar(255) not null,
@@ -7,7 +8,14 @@ CREATE TABLE if NOT EXISTS UserProfile(
 );
 
 
+INSERT INTO Users(id,user_name,first_name,last_name,phone_number,email_address)
+VALUES
+(1, 'JonDoe','Jon','Doe','+63-904-471-1975','jondoe@email.com'),
+(2, 'JaneDoe','Jane','Doe','+63-978-748-3675','janedoe@email.com');
+
 UPDATE Movie SET cinema_id=1 WHERE id=5;
 UPDATE Movie SET cinema_id=2 WHERE id=6;
 UPDATE Movie SET cinema_id=1 WHERE id=7;
 UPDATE Movie SET cinema_id=2 WHERE id=8;
+
+
