65bafcd825233912f400460ff808ec3c178c5112
CANUTLO <lorenz.alvin.canuto@oocl.com>
2021-08-25 15:09:02 +0800

    [Backend][Lorenz] added sql file to update movie poster to working links

---
+++ src/main/resources/db/migration/V1.7__update_movie_poster_column.sql
@@ -0,0 +1,8 @@
+UPDATE Movie SET poster='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/d1pklzbuyaab0la-1552597012.jpg' WHERE id=1;
+UPDATE Movie SET poster='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/d1pklzbuyaab0la-1552597012.jpg' WHERE id=2;
+UPDATE Movie SET poster='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/d1pklzbuyaab0la-1552597012.jpg' WHERE id=3;
+UPDATE Movie SET poster='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/d1pklzbuyaab0la-1552597012.jpg' WHERE id=4;
+UPDATE Movie SET poster='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/d1pklzbuyaab0la-1552597012.jpg' WHERE id=5;
+UPDATE Movie SET poster='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/d1pklzbuyaab0la-1552597012.jpg' WHERE id=6;
+UPDATE Movie SET poster='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/d1pklzbuyaab0la-1552597012.jpg' WHERE id=7;
+UPDATE Movie SET poster='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/d1pklzbuyaab0la-1552597012.jpg' WHERE id=8;
